5a1d5d3e51c2f9ecf7e74bfa03b3ac8ced0cbebb
parth141093 <parthpatel1410@gmail.com>
2024-04-12 11:37:11 +0300

    done

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.get('/foods/:id', async (req,res) => {
     const id = req.params.id;
     const food = await Food.findById(id);
     res.json(food);
+});
 // Update data
 app.patch('/update-food/:id', async (req, res) => {
     try {
