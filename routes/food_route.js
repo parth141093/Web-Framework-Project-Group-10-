887e9baddfa9a2310781e5cef746040b2ba60b06
thanh22000 <onnellinentc@gmail.com>
2024-04-23 07:21:46 +0300

    add some comment to food_route

--- routes/food_route.js
+++ routes/food_route.js
@@ -4,24 +4,23 @@ const router = express.Router();
 
 router.get('/', food_controller.getHome);
 
+// Search
 router.get('/food/search', food_controller.searchFood);
 
+// Add
 router.get('/food/add', food_controller.getAddFoodPage);
-
 router.post('/food', food_controller.postFood);
 
+// List
 router.get('/food', food_controller.getAllFood);
-
 router.get('/food/type/:type_of_food', food_controller.getAllFoodByType);
-
 router.get('/food/:id', food_controller.getFoodById);
 
 // Edit
 router.get('/food/edit/:id', food_controller.getEditFoodPage);
 router.post('/edit/:id', food_controller.editFood);
 
-
-
+// Delete
 router.post('/food/delete/:id', food_controller.deleteFood);
 
 
